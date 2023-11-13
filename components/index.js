1b54c02391f773165d2eb5490fece0ce2da8d421
gift56 <efeasiughu@gmail.com>
2023-11-13 18:11:00 -0500

    creating pages

--- components/index.js
+++ components/index.js
@@ -1,1 +1,27 @@
-export {}
+import AuthLink from "./AuthLink";
+import Card from "./Card";
+import CardList from "./CardList";
+import CategoryList from "./CategoryList";
+import Comments from "./Comments";
+import Featured from "./Featured";
+import Footer from "./Footer";
+import Menu from "./Menu";
+import MenuCategories from "./MenuCategories";
+import MenuPost from "./MenuPost";
+import Navbar from "./Navbar";
+import Pagination from "./Pagination";
+
+export {
+  AuthLink,
+  Card,
+  CardList,
+  CategoryList,
+  Comments,
+  Featured,
+  Footer,
+  Menu,
+  MenuCategories,
+  MenuPost,
+  Navbar,
+  Pagination,
+};
