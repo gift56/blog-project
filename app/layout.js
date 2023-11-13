77444b49784f4610cdeb4bba3f813d786ad0ea08
gift56 <efeasiughu@gmail.com>
2023-11-13 17:08:05 -0500

    working on my blog project

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Dev Blog',
-  description: 'The best blog app!',
-}
+  title: "Dev Blog",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="min-h-screen bg-softbg text-dark">
+          <div className="lg:max-w-[1536px] mx-auto">{children}</div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
