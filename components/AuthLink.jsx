d9bf783d4176b77105d3649528401e1971f26cfe
gift56 <efeasiughu@gmail.com>
2023-11-14 18:35:44 -0500

    working on navbar

--- components/AuthLink.jsx
+++ components/AuthLink.jsx
@@ -1,11 +1,51 @@
 "use client";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 
 const AuthLink = () => {
-  const status = "Notauthenticated";
+  const status = "notauthenticated";
+  const [mobileNav, setMobileNav] = useState(false);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setMobileNav(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const socialIcons = [
+    "/facebook.png",
+    "/instagram.png",
+    "/tiktok.png",
+    "/youtube.png",
+  ];
+  const navLinks = [
+    {
+      to: "/",
+      text: "Homepage",
+    },
+    {
+      to: "/",
+      text: "Contact",
+    },
+    {
+      to: "/",
+      text: "About",
+    },
+  ];
+
   return (
     <>
-      {status === "Notauthenticated" ? (
+      {status === "notauthenticated" ? (
         <Link
           href="/login"
           className="hidden md:flex hover:border-b-2 h-full border-black transition-all text-lg font-normal"
@@ -28,6 +68,50 @@ const AuthLink = () => {
           </span>
         </>
       )}
+      <span onClick={() => setMobileNav(true)} className="text-dark md:hidden">
+        {mobileNav === false && <FaBars size={25} />}
+      </span>
+      <div
+        className={`${
+          mobileNav ? "left-0" : "-left-[180%]"
+        } md:hidden flex flex-col h-screen bg-black/40 gap-7 absolute top-0 w-full z-40 transition-all duration-300`}
+      >
+        <div
+          ref={modalRef}
+          className="flex flex-col h-full bg-white gap-5 p-4 w-[250px]"
+        >
+          <div className="w-full flex items-center justify-end">
+            <span className="text-dark" onClick={() => setMobileNav(false)}>
+              <MdClose size={25} />
+            </span>
+          </div>
+
+          <nav className="flex flex-col items-start justify-start gap-6 w-full">
+            {navLinks.map((item, index) => (
+              <Link
+                href={item.to}
+                key={index}
+                onClick={() => setMobileNav(false)}
+                className="text-lg font-normal"
+              >
+                {item.text}
+              </Link>
+            ))}
+            {status === "notauthenticated" ? (
+              <Link href="/login" className="text-lg font-normal">
+                Login
+              </Link>
+            ) : (
+              <>
+                <Link href="/write" className="text-lg font-normal">
+                  Write
+                </Link>
+                <span className="text-lg font-normal">Logout</span>
+              </>
+            )}
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
