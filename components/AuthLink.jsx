aec11bc641501cdec915afc6863976621e19a2d2
gift56 <efeasiughu@gmail.com>
2023-11-14 18:22:28 -0500

    working on navbar

--- components/AuthLink.jsx
+++ components/AuthLink.jsx
@@ -1,6 +1,26 @@
+"use client";
+import Link from "next/link";
+
 const AuthLink = () => {
   const status = "Notauthenticated";
-  return <div>AuthLink</div>;
+  return (
+    <>
+      {status === "Notauthenticated" ? (
+        <Link href="/login" className="">
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className="">
+            Write
+          </Link>
+          <span className="" onClick={signOut}>
+            Logout
+          </span>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLink;
