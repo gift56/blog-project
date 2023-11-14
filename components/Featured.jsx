77e8b12abbd7d6229dfa44c5c8a20ccdf151c06d
gift56 <efeasiughu@gmail.com>
2023-11-14 18:53:23 -0500

    working on featured section

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Featured = () => {
   return (
-    <section className='pt-10'>Featured</section>
-  )
-}
+    <section className="pt-5">
+      <h1 className="text-3xl md:text-5xl lg:text-7xl font-normal">
+        <b>Hey 👋, Efe's here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className="pt-5 flex items-center gap-7"></div>
+    </section>
+  );
+};
 
-export default Featured
+export default Featured;
