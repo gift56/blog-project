1551088b3286ae803acf37d559d2e94a07ba35ba
gift56 <efeasiughu@gmail.com>
2023-11-14 18:40:26 -0500

    working on navbar

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
 
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  return (
+    <div className="w-10 h-6 rounded-full cursor-pointer flex items-center justify-between relative bg-white">
+      <Image src="/moon.png" alt="" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
