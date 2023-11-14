f312041f125d4816d3d98cef6c3c74dc95864a33
gift56 <efeasiughu@gmail.com>
2023-11-14 18:57:38 -0500

    working on featured section

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const Featured = () => {
   return (
-    <section className="pt-5">
+    <section className="pt-5 flex flex-col items-start justify-start gap-10 w-full">
       <h1 className="text-3xl md:text-5xl lg:text-7xl font-normal">
         <b>Hey 👋, Efe's here!</b> Discover my stories and creative ideas.
       </h1>
-      <div className="pt-5 flex items-center gap-7"></div>
+      <div className="flex flex-col lg:flex-row items-center gap-7 w-full">
+        <div className="flex-1 h-[500px] relative">
+          <Image src="/p1.jpeg" alt="" fill className="object-cover" />
+        </div>
+        <div className="flex-1 flex flex-col items-start justify-start gap-6">
+          <h3></h3>
+        </div>
+      </div>
     </section>
   );
 };
