2a79ffba5edf2e25763394fc32aeb4ac73902ee2
gift56 <efeasiughu@gmail.com>
2023-11-14 18:43:40 -0500

    working on navbar

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 
 const ThemeToggle = () => {
   return (
-    <div className="w-10 h-6 rounded-full cursor-pointer flex items-center justify-between relative bg-white">
+    <div className="w-12 h-6 rounded-full cursor-pointer flex items-center justify-between relative bg-white px-1">
       <Image src="/moon.png" alt="" width={14} height={14} />
+      <div className="w-[15px] h-[15px] rounded-full absolute right-1 bg-white"></div>
+      <Image src="/sun.png" alt="" width={14} height={14} />
     </div>
   );
 };
