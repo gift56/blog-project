84859ba36d5ea763bed739b692fd96b559890f18
gift56 <efeasiughu@gmail.com>
2023-11-14 18:23:32 -0500

    working on navbar

--- components/AuthLink.jsx
+++ components/AuthLink.jsx
@@ -6,15 +6,24 @@ const AuthLink = () => {
   return (
     <>
       {status === "Notauthenticated" ? (
-        <Link href="/login" className="">
+        <Link
+          href="/login"
+          className="hidden md:flex hover:border-b-2 h-full border-black transition-all text-lg font-normal"
+        >
           Login
         </Link>
       ) : (
         <>
-          <Link href="/write" className="">
+          <Link
+            href="/write"
+            className="hidden md:flex hover:border-b-2 h-full border-black transition-all text-lg font-normal"
+          >
             Write
           </Link>
-          <span className="" onClick={signOut}>
+          <span
+            className="hidden md:flex hover:border-b-2 h-full border-black transition-all text-lg font-normal"
+            // onClick={signOut}
+          >
             Logout
           </span>
         </>
