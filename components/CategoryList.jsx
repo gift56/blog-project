8e2aaa73b5634ece0a6f40a5bdedf7cb04320460
gift56 <efeasiughu@gmail.com>
2023-11-14 19:30:08 -0500

    working on category section

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -2,6 +2,9 @@ const CategoryList = () => {
   return (
     <section className="py-9 w-full flex flex-col items-start justify-start gap-6">
       <h3 className="text-3xl font-semibold md:text-4xl">Popular Categories</h3>
+      <div className="w-full flex flex-wrap justify-between gap-6">
+        
+      </div>
     </section>
   );
 };
