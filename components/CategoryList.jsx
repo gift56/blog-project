622ddc54cb52b47c933a49211bf9877e8d73c40f
gift56 <efeasiughu@gmail.com>
2023-11-14 19:49:32 -0500

    working on category section

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -61,22 +61,26 @@ const CategoryList = () => {
     <section className="py-9 w-full flex flex-col items-start justify-start gap-6">
       <h3 className="text-3xl font-semibold md:text-4xl">Popular Categories</h3>
       <div className="w-full flex flex-wrap justify-between gap-6">
-        <Link
-          href="/blog?cat=style"
-          className={`flex items-center gap-3 capitalize w-[50%] md:w-[40%] lg:w-[25%] xl:w-[15%] justify-center rounded-lg h-16 ${colorRender(
-            "coding"
-          )}`}
-          // key={item._id}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className="object-cover !w-10 !h-10 rounded-full"
-          />
-          <span className="text-base font-medium md:text-lg">Style</span>
-        </Link>
+        {catergoryStyles.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            key={item.slug}
+            className={`flex items-center gap-3 capitalize w-[50%] md:w-[40%] lg:w-[25%] xl:w-[15%] justify-center rounded-lg h-16 ${colorRender(
+              item.slug
+            )}`}
+          >
+            <Image
+              src={item.src}
+              alt={item.alt}
+              width={32}
+              height={32}
+              className="object-cover !w-10 !h-10 rounded-full"
+            />
+            <span className="text-base font-medium md:text-lg">
+              {item.title}
+            </span>
+          </Link>
+        ))}
       </div>
     </section>
   );
