fe627b752bb72b2330620b9957b4f0cb8711d1ea
gift56 <efeasiughu@gmail.com>
2023-11-14 19:29:39 -0500

    working on category section

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -1,7 +1,9 @@
 const CategoryList = () => {
-  return <section className="py-5 w-full">
-    
-  </section>;
+  return (
+    <section className="py-9 w-full flex flex-col items-start justify-start gap-6">
+      <h3 className="text-3xl font-semibold md:text-4xl">Popular Categories</h3>
+    </section>
+  );
 };
 
 export default CategoryList;
