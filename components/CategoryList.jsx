2950f73ba553805c7d93a7c4c48a1e1ff39f6a94
gift56 <efeasiughu@gmail.com>
2023-11-14 19:36:57 -0500

    working on category section

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const CategoryList = () => {
   const colorRender = (color) => {
     if (color === "style") {
@@ -14,13 +16,16 @@ const CategoryList = () => {
       return "bg-[#5e4fff31]";
     }
   };
+
   return (
     <section className="py-9 w-full flex flex-col items-start justify-start gap-6">
       <h3 className="text-3xl font-semibold md:text-4xl">Popular Categories</h3>
       <div className="w-full flex flex-wrap justify-between gap-6">
         <Link
           href="/blog?cat=style"
-          className={`flex items-center gap-3 capitalize w-[15%] justify-center rounded-lg h-20`}
+          className={`flex items-center gap-3 capitalize w-[15%] justify-center rounded-lg h-20 ${colorRender(
+            "style"
+          )}`}
           // key={item._id}
         ></Link>
       </div>
