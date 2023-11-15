ba520296c212353b250986124beebc827ccccf6b
gift56 <efeasiughu@gmail.com>
2023-11-14 20:03:09 -0500

    working on last section for the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
-import { CategoryList, Featured } from "@/components";
+import { CardList, CategoryList, Featured, Menu } from "@/components";
 
 export default function Home() {
   return (
     <main>
       <Featured />
       <CategoryList />
+      <section className="flex items-start justify-between gap-6">
+        <CardList />
+        <Menu />
+      </section>
     </main>
   );
 }
