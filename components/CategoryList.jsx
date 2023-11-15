da1d6380f8f7386d33a2c4d74d87f2cbef6d9f93
gift56 <efeasiughu@gmail.com>
2023-11-14 19:36:27 -0500

    working on category section

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -1,4 +1,19 @@
 const CategoryList = () => {
+  const colorRender = (color) => {
+    if (color === "style") {
+      return "bg-[#57c4ff31]";
+    } else if (color === "fashion") {
+      return "bg-[#da85c731]";
+    } else if (color === "food") {
+      return "bg-[#7fb88133]";
+    } else if (color === "travel") {
+      return "bg-[#ff795736]";
+    } else if (color === "culture") {
+      return "bg-[#ffb04f45]";
+    } else if (color === "coding") {
+      return "bg-[#5e4fff31]";
+    }
+  };
   return (
     <section className="py-9 w-full flex flex-col items-start justify-start gap-6">
       <h3 className="text-3xl font-semibold md:text-4xl">Popular Categories</h3>
