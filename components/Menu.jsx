396dcf37336210b2e3b0957642b13a9679b750ae
gift56 <efeasiughu@gmail.com>
2023-11-19 08:32:37 -0500

    working on blog card list section

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Menu = () => {
   return (
     <div className="w-full lg:flex-[0.5] flex flex-col items-start justify-start gap-6">
-      <h4 className="">{`What's Happening`}</h4>
+      <div>
+        <h4 className="text-sm font-medium md:text-base text-dark">{`What's Happening 🗞️`}</h4>
+        <h2 className="text-xl font-semibold md:text-2xl text-darkBg">
+          Most Popular
+        </h2>
+      </div>
     </div>
   );
 };
