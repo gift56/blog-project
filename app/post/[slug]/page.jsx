4f77597b56b7cd96da2135ff2e257b5a0694e6b3
gift56 <efeasiughu@gmail.com>
2023-11-19 14:16:23 -0500

    working on single post page

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -5,7 +5,7 @@ const PostDetailPage = () => {
   return (
     <main className="w-full flex flex-col items-start justify-start py-10">
       <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
-        <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-5">
+        <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-8">
           <h1 className="text-3xl font-bold md:text-5xl text-darkBg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </h1>
