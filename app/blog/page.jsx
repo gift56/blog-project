8c5b58c3aabd51bb564088f2cc44e69eaeecb1d7
gift56 <efeasiughu@gmail.com>
2023-11-19 13:25:44 -0500

    working on blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,7 @@
 const BlogPage = () => {
   return (
     <main className="w-full flex flex-col gap-8 py-10">
-      <h1></h1>
+      <h1 className="w-full text-center text-2xl font-bold md:text-4xl">Culture Blog</h1>
     </main>
   );
 };
