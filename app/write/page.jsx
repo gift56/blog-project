16d851805ca475aa1f5d4dca77dce8ee42e3c131
gift56 <efeasiughu@gmail.com>
2023-11-19 17:23:11 -0500

    working on write page

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -15,9 +15,17 @@ const WritePage = () => {
 
   const router = useRouter();
 
-  return <main className="w-full flex flex-col relative items-start justify-start gap-6">
-    
-  </main>;
+  return (
+    <main className="w-full flex flex-col relative items-start justify-start gap-6 py-14">
+      <input
+        type="text"
+        placeholder="Title"
+        className="outline-none border-none bg-transparent py-10 text-darkBg w-full px-7 text-3xl font-medium md:text-5xl placeholder:text-[#b3b3b1]"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <select onChange={(e) => setCatSlug(e.target.value)}></select>
+    </main>
+  );
 };
 
 export default WritePage;
