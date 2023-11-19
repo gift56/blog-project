8cdb426c056c5cc7c323ac092450525fcac72c75
gift56 <efeasiughu@gmail.com>
2023-11-19 12:54:08 -0500

    working on pagination

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -6,8 +6,22 @@ const Pagination = ({ page, hasPrev, hasNext }) => {
 
   return (
     <div className="w-full flex items-center justify-between gap-5">
-      <button type="button" disabled={!has} className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed">Previous</button>
-      <button type="button" disabled={!has} className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed">Next</button>
+      <button
+        type="button"
+        disabled={!hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+        className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed"
+      >
+        Previous
+      </button>
+      <button
+        type="button"
+        disabled={!hasNext}
+        onClick={() => router.push(`?page=${page + 1}`)}
+        className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed"
+      >
+        Next
+      </button>
     </div>
   );
 };
