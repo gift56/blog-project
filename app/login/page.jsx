e3e7632e37fe39e16e5e418dc755d3cae891e639
gift56 <efeasiughu@gmail.com>
2023-11-19 14:50:11 -0500

    working on single post page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="w-full flex items-center justify-center py-14">
+      <div className="flex flex-col items-center justify-center gap-6 bg-dark py-3 px-5 rounded-xl"></div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
