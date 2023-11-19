8aedb826a98e9d026caf305f6e68f332b886f9f1
gift56 <efeasiughu@gmail.com>
2023-11-19 14:29:37 -0500

    working on single post page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -6,11 +6,26 @@ import Image from "next/image";
 // import { useSession } from "next-auth/react";
 
 const Comments = () => {
+  const status = "authenticated";
   const [desc, setDesc] = useState("");
 
   return (
     <div className="w-full flex flex-col items-start justify-start gap-5">
       <h3 className="text-2xl font-bold text-dark md:text-3xl">Comments</h3>
+      {status === "authenticated" ? (
+        <div className="">
+          <textarea
+            placeholder="write a comment..."
+            className=""
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className="" onClick={() => {}}>
+            Send
+          </button>
+        </div>
+      ) : (
+        <Link href="/login">Login to write a comment</Link>
+      )}
     </div>
   );
 };
