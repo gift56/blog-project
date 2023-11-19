b16ba131c805b02b1bd4b0af9552bf4a6c494625
gift56 <efeasiughu@gmail.com>
2023-11-19 14:27:14 -0500

    working on single post page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -5,9 +5,10 @@ import Image from "next/image";
 // import useSWR from "swr";
 // import { useSession } from "next-auth/react";
 
-
 const Comments = () => {
-  return <div>Comments</div>;
+  const [desc, setDesc] = useState("");
+
+  return <div className="w-full flex flex-col items-start justify-start">Comments</div>;
 };
 
 export default Comments;
