edae5ef92a67810cbe1068fa7d8759cee86362c7
gift56 <efeasiughu@gmail.com>
2023-11-19 14:52:53 -0500

    working on login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoginPage = () => {
   return (
     <div className="w-full flex items-center justify-center py-14">
-      <div className="flex flex-col items-center justify-center gap-6 bg-dark py-3 px-5 rounded-xl">
+      <div className="flex flex-col items-center justify-center gap-6 shadow-2xl py-10 px-5 rounded-xl w-full md:w-[500px]">
         <button>Sign in with Google</button>
         <button>Sign in with Github</button>
         <button>Sign in with Facebook</button>
