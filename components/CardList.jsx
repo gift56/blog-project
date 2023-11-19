1586977a755b3f6f6dbf84a36e7150d38026ae9f
gift56 <efeasiughu@gmail.com>
2023-11-19 07:54:24 -0500

    working on blog card list section

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CardList = () => {
-  return <div className="w-full lg:flex-[2]">CardList</div>;
+  return (
+    <div className="w-full lg:flex-[2] flex flex-col items-start justify-start gap-6">
+      <h2>Recent Post</h2>
+    </div>
+  );
 };
 
 export default CardList;
