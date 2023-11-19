99cd8df7de12fcffc1fe9b0aa6f490ea0a1b8055
gift56 <efeasiughu@gmail.com>
2023-11-19 14:26:20 -0500

    working on single post page

---
+++ components/Comments.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+// import useSWR from "swr";
+// import { useSession } from "next-auth/react";
+
+
+const Comments = () => {
+  return <div>Comments</div>;
+};
+
+export default Comments;
