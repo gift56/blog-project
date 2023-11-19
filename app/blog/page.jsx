93b70aa99c09083369d82bd20e576b2a66718bc9
gift56 <efeasiughu@gmail.com>
2023-11-19 13:24:33 -0500

    working on blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,9 @@
 const BlogPage = () => {
-  return <main>BlogPage</main>;
+  return (
+    <main className="w-full flex flex-col gap-8 py-10">
+      <h1></h1>
+    </main>
+  );
 };
 
 export default BlogPage;
