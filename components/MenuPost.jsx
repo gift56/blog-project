f64226641bfd5047132b3ea25fcfd0c46cd73501
gift56 <efeasiughu@gmail.com>
2023-11-19 08:57:05 -0500

    working on blog card list section

--- components/MenuPost.jsx
+++ components/MenuPost.jsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 const MenuPost = ({ withImage }) => {
-  return <div>MenuPost</div>;
+  return <div className="w-full flex flex-col items-start justify-start gap-5">
+
+  </div>;
 };
 
 export default MenuPost;
