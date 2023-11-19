d84e019eba8ebc74bbc609c68d0b765ce5f85b1e
gift56 <efeasiughu@gmail.com>
2023-11-19 07:58:41 -0500

    working on blog card list section

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Pagination } from ".";
 
 const CardList = () => {
   return (
     <div className="w-full lg:flex-[2] flex flex-col items-start justify-start gap-6">
-      <h2>Recent Post</h2>
+      <h2 className="text-2xl font-bold md:text-3xl">Recent Post</h2>
+      <div className="w-full flex flex-col items-start justify-start gap-8">
+        <div className="w-full flex flex-col md:flex-row items-start justify-start gap-8 md:items-center">
+          
+        </div>
+      </div>
+      <Pagination />
     </div>
   );
 };
