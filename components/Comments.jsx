f03b45813aafca53c7c92936f0ef80d1e10f7b0b
gift56 <efeasiughu@gmail.com>
2023-11-19 14:31:17 -0500

    working on single post page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -13,13 +13,17 @@ const Comments = () => {
     <div className="w-full flex flex-col items-start justify-start gap-5">
       <h3 className="text-2xl font-bold text-dark md:text-3xl">Comments</h3>
       {status === "authenticated" ? (
-        <div className="">
+        <div className="w-full flex items-end justify-between gap-5">
           <textarea
             placeholder="write a comment..."
             className=""
             onChange={(e) => setDesc(e.target.value)}
           />
-          <button className="" onClick={() => {}}>
+          <button
+            type="submit"
+            className="w-[100px] h-12 border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:border-[#dc143c79] disabled:cursor-not-allowed transition-all duration-300"
+            onClick={() => {}}
+          >
             Send
           </button>
         </div>
