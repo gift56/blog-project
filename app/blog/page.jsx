1f7b1388bf91010b3ceaa556acb1adcdf9460f47
gift56 <efeasiughu@gmail.com>
2023-11-19 13:26:39 -0500

    working on blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,15 @@
+import { CardList, Menu } from "@/components";
+
 const BlogPage = () => {
   return (
     <main className="w-full flex flex-col gap-8 py-10">
-      <h1 className="w-full text-center text-2xl font-bold md:text-4xl">Culture Blog</h1>
+      <h1 className="w-full text-center text-2xl font-bold md:text-4xl">
+        Culture Blog
+      </h1>
+      <section className="flex flex-col lg:flex-row items-start justify-between gap-10 py-8">
+        <CardList />
+        <Menu />
+      </section>
     </main>
   );
 };
