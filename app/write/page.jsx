1850c3b2f14b099a9c4e387485ccc4c6f4086e84
gift56 <efeasiughu@gmail.com>
2023-11-19 17:18:19 -0500

    working on write page

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,7 +1,23 @@
 "use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.bubble.css";
 
 const WritePage = () => {
-  return <div>WritePage</div>;
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const [media, setMedia] = useState("");
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [catSlug, setCatSlug] = useState("");
+
+  const router = useRouter();
+
+  return <main className="w-full flex flex-col relative items-start justify-start gap-6">
+    
+  </main>;
 };
 
 export default WritePage;
