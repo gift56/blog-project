60b7dd4f4709021a72672f867bbd442ebb5cf325
gift56 <efeasiughu@gmail.com>
2023-11-19 08:00:39 -0500

    working on blog card list section

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Pagination } from ".";
+import Image from "next/image";
 
 const CardList = () => {
   return (
@@ -7,7 +8,18 @@ const CardList = () => {
       <h2 className="text-2xl font-bold md:text-3xl">Recent Post</h2>
       <div className="w-full flex flex-col items-start justify-start gap-8">
         <div className="w-full flex flex-col md:flex-row items-start justify-start gap-8 md:items-center">
-          
+          <div className="w-full lg:flex-1 relative">
+            <Image
+              src="/p1.jpeg"
+              alt="island"
+              width={500}
+              height={500}
+              className="!w-full md:!h-[300px] rounded-lg"
+            />
+          </div>
+          <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-6">
+
+          </div>
         </div>
       </div>
       <Pagination />
