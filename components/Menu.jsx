854f06c3d10afae8f366f29849d1e86368fa62e8
gift56 <efeasiughu@gmail.com>
2023-11-19 08:29:31 -0500

    working on blog card list section

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Menu = () => {
-  return <div className="w-full lg:flex-[0.5]">Menu</div>;
+  return (
+    <div className="w-full lg:flex-[0.5] flex flex-col items-start justify-start gap-6">
+      <h4 className="">{`What's Happening`}</h4>
+    </div>
+  );
 };
 
 export default Menu;
