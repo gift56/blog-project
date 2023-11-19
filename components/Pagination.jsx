2f26eb42e115fdfc9ebfaad1b2afe326dc91b10b
gift56 <efeasiughu@gmail.com>
2023-11-19 12:49:22 -0500

    working on pagination

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,8 +1,15 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full flex items-center justify-between gap-5">
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;
