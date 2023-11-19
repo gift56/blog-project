fbe2705cee4a58fc3b7be15f5f113363a32f0eac
gift56 <efeasiughu@gmail.com>
2023-11-19 14:54:50 -0500

    working on login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,7 @@ const LoginPage = () => {
   return (
     <div className="w-full flex items-center justify-center py-14">
       <div className="flex flex-col items-center justify-center gap-6 shadow-2xl py-10 px-5 rounded-xl w-full md:w-[500px]">
-        <button>Sign in with Google</button>
+        <button type="button" className="w-fit bg-white text-darkBg py-4 px-10 rounded-lg font-medium text-lg md:text-xl">Sign in with Google</button>
         <button>Sign in with Github</button>
         <button>Sign in with Facebook</button>
       </div>
