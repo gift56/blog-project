901305ff469ce917caac09c2dc780c3f1c351563
gift56 <efeasiughu@gmail.com>
2023-11-19 08:04:39 -0500

    working on blog card list section

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="w-full flex flex-col md:flex-row items-start justify-start gap-8 md:items-center">
+      <div className="w-full lg:flex-1 relative">
+        <Image
+          src="/p1.jpeg"
+          alt="island"
+          width={500}
+          height={500}
+          className="!w-full md:!h-[300px] rounded-lg"
+        />
+      </div>
+      <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-6">
+        <p></p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,26 +1,14 @@
 import React from "react";
-import { Pagination } from ".";
-import Image from "next/image";
+import { Card, Pagination } from ".";
 
 const CardList = () => {
   return (
     <div className="w-full lg:flex-[2] flex flex-col items-start justify-start gap-6">
       <h2 className="text-2xl font-bold md:text-3xl">Recent Post</h2>
       <div className="w-full flex flex-col items-start justify-start gap-8">
-        <div className="w-full flex flex-col md:flex-row items-start justify-start gap-8 md:items-center">
-          <div className="w-full lg:flex-1 relative">
-            <Image
-              src="/p1.jpeg"
-              alt="island"
-              width={500}
-              height={500}
-              className="!w-full md:!h-[300px] rounded-lg"
-            />
-          </div>
-          <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-6">
-
-          </div>
-        </div>
+        <Card />
+        <Card />
+        <Card />
       </div>
       <Pagination />
     </div>
