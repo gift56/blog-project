cd33a4588254e869abece56b2a3673ef80c4b89f
gift56 <efeasiughu@gmail.com>
2023-11-19 13:03:56 -0500

    working on pagination

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
+  const socialIcons = [
+    "/facebook.png",
+    "/instagram.png",
+    "/tiktok.png",
+    "/youtube.png",
+  ];
   return (
     <footer className="w-full py-10 flex flex-col md:flex-row items-start justify-start gap-8 md:justify-between">
-      
+      <div className="flex flex-col items-start justify-start gap-4">
+        <div className="flex items-center justify-center">
+          <Link
+            href="/"
+            className="select-none text-2xl font-bold md:text-3xl text-black"
+          >
+            Devblog
+          </Link>
+        </div>
+        <p className="text-sm font-normal md:text-base text-dark">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+          necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
+          porro sequi, totam minima consequuntur, aspernatur deleniti vero
+          repellendus dorales.
+        </p>
+        <div className="flex items-center justify-start gap-2">
+          {socialIcons.map((item, index) => (
+            <Image
+              src={item}
+              alt="social icons"
+              width={24}
+              height={24}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+      <div></div>
     </footer>
   );
 };
