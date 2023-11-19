216203ebe24ebc0850fa363830eea969ee821cdf
gift56 <efeasiughu@gmail.com>
2023-11-19 12:53:33 -0500

    working on pagination

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -6,8 +6,8 @@ const Pagination = ({ page, hasPrev, hasNext }) => {
 
   return (
     <div className="w-full flex items-center justify-between gap-5">
-      <button>Previous</button>
-      <button>Next</button>
+      <button type="button" disabled={!has} className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed">Previous</button>
+      <button type="button" disabled={!has} className="w-[100px] border border-primary bg-primary text-white disabled:bg-[#dc143c79] disabled:cursor-not-allowed">Next</button>
     </div>
   );
 };
