e0191d922167a56b6a4b6853c66d458ec8da5b51
gift56 <efeasiughu@gmail.com>
2023-11-19 14:02:34 -0500

    working on single post page

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const PostPage = () => {
+const PostDetailPage = () => {
   return <main>PostPage</main>;
 };
 
-export default PostPage;
+export default PostDetailPage;
