22fa7239030d5ef9a54c611c4e348ba3203d589f
gift56 <efeasiughu@gmail.com>
2023-11-20 12:06:02 -0500

    working on auth apis

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer, Navbar } from "@/components";
+import AuthProvider from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen bg-softbg text-black">
-          <div className="w-[90%] lg:w-[85%] mx-auto">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </main>
+        <AuthProvider>
+          <main className="min-h-screen bg-softbg text-black">
+            <div className="w-[90%] lg:w-[85%] mx-auto">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
