f621a28e7ef86d2e42fae2576bd60ce854e86bfe
gift56 <efeasiughu@gmail.com>
2023-11-20 13:38:58 -0500

    working on auth apis on login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,4 +1,8 @@
+import { useSession } from "next-auth/react";
+
 const LoginPage = () => {
+  const { data, status } = useSession();
+
   return (
     <div className="w-full flex items-center justify-center py-14">
       <div className="flex flex-col items-center justify-center gap-6 shadow-2xl py-10 px-5 rounded-xl w-full md:w-[500px]">
