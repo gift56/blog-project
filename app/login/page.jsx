8c534642df2e9379c8e53a77ed58db40eb76d1b1
gift56 <efeasiughu@gmail.com>
2023-11-20 13:52:47 -0500

    working on auth apis on login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,19 @@
 "use client";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
-  const { data, status } = useSession();
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className="text-lg font-medium text-darkBg">Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/")
+  }
+
   return (
     <div className="w-full flex items-center justify-center py-14">
       <div className="flex flex-col items-center justify-center gap-6 shadow-2xl py-10 px-5 rounded-xl w-full md:w-[500px]">
