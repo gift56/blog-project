da3049e03e8f0e80b1121f93b6bfabebc733180c
gift56 <efeasiughu@gmail.com>
2023-11-20 18:23:24 -0500

    working on post endpoint

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import { CardList, CategoryList, Featured, Menu } from "@/components";
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
   return (
     <main>
       <Featured />
       <CategoryList />
       <section className="flex flex-col lg:flex-row items-start justify-between gap-10 py-8">
-        <CardList />
+        <CardList page={page} />
         <Menu />
       </section>
     </main>
