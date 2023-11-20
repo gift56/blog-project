14514e367ab98fb513e8f172f9614ebaf0d4bb31
gift56 <efeasiughu@gmail.com>
2023-11-20 13:37:52 -0500

    working on auth apis on login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-
 const LoginPage = () => {
   return (
     <div className="w-full flex items-center justify-center py-14">
       <div className="flex flex-col items-center justify-center gap-6 shadow-2xl py-10 px-5 rounded-xl w-full md:w-[500px]">
-        <button type="button" className="w-fit bg-white text-darkBg py-4 px-10 rounded-lg font-medium text-lg md:text-xl">Sign in with Google</button>
-        <button className="w-fit bg-darkBg text-white py-4 px-10 rounded-lg font-medium text-lg md:text-xl">Sign in with Github</button>
-        <button className="w-fit bg-[#087bea] text-white py-4 px-6 md:px-10 rounded-lg font-medium text-lg md:text-xl">Sign in with Facebook</button>
+        <button
+          type="button"
+          className="w-fit bg-white text-darkBg py-4 px-10 rounded-lg font-medium text-lg md:text-xl"
+        >
+          Sign in with Google
+        </button>
+        <button className="w-fit bg-darkBg text-white py-4 px-10 rounded-lg font-medium text-lg md:text-xl">
+          Sign in with Github
+        </button>
+        <button className="w-fit bg-[#087bea] text-white py-4 px-6 md:px-10 rounded-lg font-medium text-lg md:text-xl">
+          Sign in with Facebook
+        </button>
       </div>
     </div>
   );
