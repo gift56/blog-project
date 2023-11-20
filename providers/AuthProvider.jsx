13c70fff99771533b539e70754c8d3ad74c5198a
gift56 <efeasiughu@gmail.com>
2023-11-20 12:05:14 -0500

    working on auth apis

--- providers/AuthProvider.jsx
+++ providers/AuthProvider.jsx
@@ -1,5 +1,9 @@
-const AuthProvider = () => {
-  return <div>AuthProvider</div>;
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+const AuthProvider = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
