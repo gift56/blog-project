cc43de13d5baa4fd0f55699db5444d4bb007edde
gift56 <efeasiughu@gmail.com>
2023-11-20 15:05:19 -0500

    generated prisma

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-// import prisma from "./connect";
+import prisma from "./connect";
 import { getServerSession } from "next-auth";
 
 export const authOptions = {
