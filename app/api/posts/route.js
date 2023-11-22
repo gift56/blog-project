14d2991cb9338e801326cdcaf2a77c3b0a0e49f6
gift56 <efeasiughu@gmail.com>
2023-11-22 13:16:40 -0500

    all endpoints are working fine

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -35,26 +35,26 @@ export const GET = async (req) => {
 };
 
 // CREATE A POST
-// export const POST = async (req) => {
-//   const session = await getAuthSession();
+export const POST = async (req) => {
+  const session = await getAuthSession();
 
-//   if (!session) {
-//     return new NextResponse(
-//       JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
-//     );
-//   }
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
 
-//   try {
-//     const body = await req.json();
-//     const post = await prisma.post.create({
-//       data: { ...body, userEmail: session.user.email },
-//     });
+  try {
+    const body = await req.json();
+    const post = await prisma.post.create({
+      data: { ...body, userEmail: session.user.email },
+    });
 
-//     return new NextResponse(JSON.stringify(post, { status: 200 }));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-//     );
-//   }
-// };
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
