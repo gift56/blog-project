c2608104021232a623340ffea45b69843bc30d4f
gift56 <efeasiughu@gmail.com>
2023-11-23 07:18:42 -0500

    working on adding product

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,13 +1,16 @@
 import { CardList, Menu } from "@/components";
 
-const BlogPage = () => {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
   return (
     <main className="w-full flex flex-col gap-12 py-10">
       <h1 className="w-full text-center text-2xl font-bold md:text-4xl">
-        Culture Blog
+        <span className="capitalize">{cat}</span> Blog
       </h1>
       <section className="flex flex-col lg:flex-row items-start justify-between gap-10 py-8">
-        <CardList />
+        <CardList page={page} cat={cat} />
         <Menu />
       </section>
     </main>
