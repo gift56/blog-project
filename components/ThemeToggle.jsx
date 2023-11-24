e5bf44a174f769278e8ae9b51613ff8d20934a6e
gift56 <efeasiughu@gmail.com>
2023-11-24 03:38:56 -0500

    working on dark theme

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 const ThemeToggle = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  
+  const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <div className="w-20 h-8 rounded-full cursor-pointer flex items-center justify-between relative bg-white px-2">
       <Image src="/moon.png" alt="moon" width={18} height={18} />
