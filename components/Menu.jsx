a9526b655ea84b4343d618f387646773ace7d3bc
gift56 <efeasiughu@gmail.com>
2023-11-24 08:08:32 -0500

    working on dark theme

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -5,26 +5,26 @@ const Menu = () => {
   return (
     <div className="w-full lg:flex-[0.8] flex flex-col items-start justify-start gap-6">
       <div>
-        <h4 className="text-sm font-medium md:text-base text-dark">{`What's Happening 🗞️`}</h4>
-        <h2 className="text-xl font-bold md:text-2xl text-darkBg">
+        <h4 className="text-sm font-medium md:text-base text-dark dark:text-[#C0C5D0]">{`What's Happening 🗞️`}</h4>
+        <h2 className="text-xl font-bold md:text-2xl">
           Most Popular
         </h2>
       </div>
       <MenuPost />
       <div>
-        <h4 className="text-sm font-medium md:text-base text-dark">
+        <h4 className="text-sm font-medium md:text-base text-dark dark:text-[#C0C5D0]">
           Discover by topic
         </h4>
-        <h2 className="text-xl font-bold md:text-2xl text-darkBg">
+        <h2 className="text-xl font-bold md:text-2xl">
           Categories
         </h2>
       </div>
       <MenuCategories />
       <div>
-        <h4 className="text-sm font-medium md:text-base text-dark">
+        <h4 className="text-sm font-medium md:text-base text-dark dark:text-[#C0C5D0]">
           Chosen by the author
         </h4>
-        <h2 className="text-xl font-bold md:text-2xl text-darkBg">
+        <h2 className="text-xl font-bold md:text-2xl">
           Editors Pick
         </h2>
       </div>
--- components/MenuCategories.jsx
+++ components/MenuCategories.jsx
@@ -1,60 +1,48 @@
 import Link from "next/link";
 import React from "react";
 
-const MenuCategories = () => {
-  const catLinks = [
-    {
-      href: "/blog?cat=style",
-      text: "Style",
-    },
-    {
-      href: "/blog",
-      text: "Fashion",
-    },
-    {
-      href: "/blog",
-      text: "Food",
-    },
-    {
-      href: "/blog",
-      text: "Travel",
-    },
-    {
-      href: "/blog",
-      text: "Culture",
-    },
-    {
-      href: "/blog",
-      text: "Coding",
-    },
-  ];
+const getData = async () => {
+  const res = await fetch(`${baseUrl}/api/categories`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const MenuCategories = async () => {
+  const data = await getData();
 
   const colorRender = (color) => {
-    if (color === "Style") {
+    if (color === "style") {
       return "bg-[#57c4ff31]";
-    } else if (color === "Fashion") {
+    } else if (color === "fashion") {
       return "bg-[#da85c731]";
-    } else if (color === "Food") {
+    } else if (color === "food") {
       return "bg-[#7fb88133]";
-    } else if (color === "Travel") {
+    } else if (color === "travel") {
       return "bg-[#ff795736]";
-    } else if (color === "Culture") {
+    } else if (color === "culture") {
       return "bg-[#ffb04f45]";
-    } else if (color === "Coding") {
+    } else if (color === "coding") {
       return "bg-[#5e4fff31]";
     }
   };
+
   return (
     <div className="w-full flex flex-wrap justify-start gap-4">
-      {catLinks.map((item) => (
+      {data?.map((item) => (
         <Link
-          href={item.href}
-          key={item.text}
+          href={`/blog?cat=${item.slug}`}
+          key={item.id}
           className={`flex items-center gap-3 capitalize w-fit px-4 justify-center rounded-lg h-12 text-base font-medium md:text-lg ${colorRender(
-            item.text
+            item.slug
           )}`}
         >
-          {item.text}
+          {item.title}
         </Link>
       ))}
     </div>
