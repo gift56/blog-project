8eb9155f3a2cca1024633b438b4cec3d637b024d
gift56 <efeasiughu@gmail.com>
2023-11-24 10:27:48 -0500

    adding data

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,27 +1,65 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { Card, Pagination } from ".";
 import { baseUrl } from "@/utils/config";
 
-const getData = async (page, cat) => {
-  const res = await fetch(
-    `${baseUrl}/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
-    }
-  );
+// const getData = async (page, cat) => {
+//   const res = await fetch(
+//     `${baseUrl}/api/posts?page=${page}&cat=${cat || ""}`,
+//     {
+//       cache: "no-store",
+//     }
+//   );
 
-  if (!res.ok) {
-    throw new Error("Failed");
+//   if (!res.ok) {
+//     throw new Error("Failed");
+//   }
+
+//   const data = await res.json();
+
+//   const reversedPosts = data.posts.reverse();
+//   return { ...data, posts: reversedPosts };
+// };
+
+const CardList = ({ page, cat }) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `${baseUrl}/api/posts?page=${page}&cat=${cat || ""}`,
+          {
+            cache: "no-store",
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed");
+        }
+
+        const newData = await res.json();
+        if (newData.posts) {
+          newData.posts.reverse();
+        }
+        setData(newData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page, cat]);
+
+  if (isLoading) {
+    return <p className="w-full lg:flex-[2]">Loading...</p>;
   }
 
-  const data = await res.json();
-
-  const reversedPosts = data.posts.reverse();
-  return { ...data, posts: reversedPosts };
-};
-
-const CardList = async ({ page, cat }) => {
-  const { posts, count } = await getData(page, cat);
+  const { posts, count } = data;
 
   const POST_PER_PAGE = 8;
 
