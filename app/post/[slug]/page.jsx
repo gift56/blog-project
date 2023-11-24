6900d9c104bd6d8607fa2183b1d769eba10e58c9
gift56 <efeasiughu@gmail.com>
2023-11-23 20:16:50 -0500

    working on adding product

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -24,32 +24,23 @@ const PostDetailPage = async ({ params }) => {
       <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
         <div className="w-full lg:flex-1 flex flex-col items-start justify-start gap-8 md:gap-10">
           <h1 className="text-3xl font-bold md:text-5xl text-darkBg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {data?.title}
           </h1>
           <div className="w-full flex items-start justify-start gap-3">
-            <div className="w-12 h-12 md:w-14 md:h-14">
-              <Image
-                src="/p1.jpeg"
-                alt="author Profile Image"
-                width={70}
-                height={70}
-                className="object-cover !w-full !h-full rounded-full"
-              />
-            </div>
-
-            {/* {data?.user?.image && (
-              <div className={styles.userImageContainer}>
+            {data?.user?.image && (
+              <div className="w-12 h-12 md:w-14 md:h-14">
                 <Image
                   src={data.user.image}
-                  alt=""
-                  fill
-                  className={styles.avatar}
+                  alt="author Profile Image"
+                  width={70}
+                  height={70}
+                  className="object-cover !w-full !h-full rounded-full"
                 />
               </div>
-            )} */}
+            )}
             <div className="flex flex-col items-start justify-start gap-1">
               <span className="text-lg font-medium md:text-xl text-dark">
-                John Doe
+                {data?.user.name}
               </span>
               <span className="text-sm font-normal text-dark">
                 25 April 2023
@@ -57,25 +48,24 @@ const PostDetailPage = async ({ params }) => {
             </div>
           </div>
         </div>
-        <div className="w-full lg:flex-1">
-          <Image
-            src="/p1.jpeg"
-            alt="island"
-            width={500}
-            height={500}
-            className="!w-full md:!h-[300px] rounded-lg object-cover"
-          />
-        </div>
+        {data?.img && (
+          <div className="w-full lg:flex-1">
+            <Image
+              src={data.img}
+              alt={data?.title}
+              width={500}
+              height={500}
+              className="!w-full md:!h-[300px] rounded-lg object-cover"
+            />
+          </div>
+        )}
       </div>
       <div className="w-full flex flex-col lg:flex-row items-start justify-start gap-6">
         <div className="w-full lg:flex-[2] flex flex-col items-start justify-start gap-6">
-          <p className="text-base font-normal text-gray md:text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            excepturi dolores aliquam unde, quae accusamus doloribus molestiae
-            voluptatum non saepe reiciendis sunt dignissimos alias tenetur,
-            ipsum possimus, quis quo illo eligendi blanditiis ad dicta.
-            Quibusdam quae architecto facere velit dignissimos.
-          </p>
+          <div
+            className="text-base font-normal text-gray md:text-lg"
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
           <Comments />
         </div>
         <Menu />
