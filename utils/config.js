ad34c64f189efa11749b1a6383c99fa666e2c623
gift56 <efeasiughu@gmail.com>
2023-11-25 04:31:04 -0500

    working on some stuff

--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,5 @@
-export const baseUrl = process.env.NEXTAUTH_URL;
+const developmentBaseUrl = "http://localhost:4000";
+const productionBaseUrl = "https://your-production-url.com"; // Replace with your actual production URL
+
+
+export const baseUrl =  process.env.NODE_ENV === 'production' ? productionBaseUrl : developmentBaseUrl;;
