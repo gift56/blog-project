a4f8263a2ab1d94fa62fffe348c21be34a2bced9
gift56 <efeasiughu@gmail.com>
2023-11-25 05:41:47 -0500

    change production url

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const developmentBaseUrl = "http://localhost:4000";
-const productionBaseUrl = "https://dev-blog-p00h.onrender.com"; // Replace with your actual production URL
+const productionBaseUrl = "https://devblogupdates.vercel.app"; // Replace with your actual production URL
 
 
 export const baseUrl =  process.env.NODE_ENV === 'production' ? productionBaseUrl : developmentBaseUrl;;
